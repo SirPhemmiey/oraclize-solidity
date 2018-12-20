8e6bf593cf61c7d18a4a2814a7c7aea435cc5a83
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-20 10:50:18 +0100

    update README.md

--- OraclizeContract.sol
+++ OraclizeContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.23;
 import "./Oraclize.sol";
 
 
@@ -8,7 +8,24 @@ contract OraclizeSolidity is usingOraclize {
     event LogPriceCalculated(string price);
     event LogNewOraclizeQuery(string description);
 
-    constructor() payable {
+    constructor() public payable {
+        emit LogContructorInitiated("Contructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
+    }
 
+    function updatePrice() public payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            emit LogNewOraclizeQuery("Oraclize was not sent, please add some ETH to cover for the query fee");
+        }
+        else {
+            oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
+            emit LogNewOraclizeQuery("Oraclize was sent, standing by the former answer");
+        }
+    }
+
+    //callback
+    function __callback(bytes32 myId, string result){
+        require(msg.sender != oraclize_cbAddress(), "Invalid Address");
+        ETHXBT = result;
+        emit LogPriceCalculated(result);
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
-#Simple-Oraclize-Solidity
+# Simple-Oraclize-Solidity
 
 A simple use case of [Oraclize](http://www.oraclize.it/) and [Solidity](http://solidity.readthedocs.io/)
